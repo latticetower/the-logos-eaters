c04d52bbffc88c2eb486482a99cbef96b31c5eb7
Michael Gamov <nofater@gmail.com>
2019-09-29 00:12:44 +0300

    Add categories selector

--- tassv1/src/panels/Home.js
+++ tassv1/src/panels/Home.js
@@ -1,32 +1,29 @@
-import React, { useState, useEffect } from 'react';
+import React, {useState, useEffect} from 'react';
 import PropTypes from 'prop-types';
 import Panel from '@vkontakte/vkui/dist/components/Panel/Panel';
 import PanelHeader from '@vkontakte/vkui/dist/components/PanelHeader/PanelHeader';
+import PanelHeaderContent from '@vkontakte/vkui/dist/components/PanelHeaderContent/PanelHeaderContent';
+import HeaderContext from '@vkontakte/vkui/dist/components/HeaderContext/HeaderContext';
 import Button from '@vkontakte/vkui/dist/components/Button/Button';
 import Group from '@vkontakte/vkui/dist/components/Group/Group';
 import Cell from '@vkontakte/vkui/dist/components/Cell/Cell';
 import Div from '@vkontakte/vkui/dist/components/Div/Div';
+import List from '@vkontakte/vkui/dist/components/List/List';
 import Avatar from '@vkontakte/vkui/dist/components/Avatar/Avatar';
 import UsersStack from '@vkontakte/vkui/dist/components/UsersStack/UsersStack';
+import Select from '@vkontakte/vkui/dist/components/Select/Select';
+import HeaderButton from '@vkontakte/vkui/dist/components/HeaderButton/HeaderButton';
+import Icon28ChevronBack from '@vkontakte/icons/dist/28/chevron_back';
+import Icon24Add from '@vkontakte/icons/dist/24/add';
+import Icon24Search from '@vkontakte/icons/dist/24/search';
+import Icon24Back from '@vkontakte/icons/dist/24/back';
+import Icon24Users from '@vkontakte/icons/dist/24/users';
+import Icon24Done from '@vkontakte/icons/dist/24/done';
+import Icon24Article from '@vkontakte/icons/dist/24/article';
+import Icon16Dropdown from '@vkontakte/icons/dist/16/dropdown';
 
 import './Home.css';
-
-const menu = [
-    {
-        url: "https://luna-2.tass.ru/",
-        categories: ['Культура', 'История'],
-        name: "Луна. Первый контакт",
-        preview: "https://luna-2.tass.ru/shares/fb.png",
-        intro: "Начало освоению спутника Земли положила «Луна-2». В 1959 году советская станция стала первым искусственным аппаратом, достигшим поверхности Луны. После этого наступило время больших открытий: запуск первых автоматических станций, первый луноход и первая высадка человека на Луну. Потом полёты к Луне на некоторое время прекратились, но сейчас внимание человечества снова обращено к ней. Мы изучили все лунные миссии: от первого успеха «Луны-2» и программы Apollo до планов новых экспедиций и строительства лунных баз."
-    },
-    {
-        url: "https://monstry.tass.ru/",
-        categories: ['Культура', 'История'],
-        name: "Бестиарий: 10 эпических монстров России",
-        preview: "https://rm-content.s3.amazonaws.com/57f773462aa31a0078116425/upload-72efc906-68ad-48eb-8e6e-872c2598c161.png",
-        intro: "Мы собрали чудовищ из разных регионов нашей страны. Они различаются по происхождению, среде обитания, внешнему виду и способностям. Единственное, что их объединяет, — колоссальные размеры."
-    }
-];
+import {menu} from '../menu'
 
 
 const Material = ({id, data}) => {
@@ -35,8 +32,11 @@ const Material = ({id, data}) => {
 
     return (
         <div>
-            <div className="collapsible" >
-                <img src={data.preview} className="preview-pic" onClick={() => {setExpanded(!expanded); console.log(expanded);} }/>
+            <div className="collapsible">
+                <img src={data.preview} className="preview-pic" onClick={() => {
+                    setExpanded(!expanded);
+                    console.log(expanded);
+                }}/>
 
                 <div className="intro" style={{height: expanded ? "auto" : "0"}}>
                     <Div>
@@ -63,10 +63,12 @@ const Material = ({id, data}) => {
 };
 
 
-const getMaterials = () => {
+const Materials = ({mode}) => {
     const items = []
 
-    for (const [index, value] of menu.entries()) {
+    const filteredMenu = mode === 'all' ? menu : menu.filter(item => item.categories.includes(mode));
+
+    for (const [index, value] of filteredMenu.entries()) {
         items.push(<Material data={value} key={index}/>)
     }
 
@@ -78,6 +80,17 @@ const getMaterials = () => {
 };
 
 const Home = ({id, go, fetchedUser}) => {
+    const [contextOpened, setContextOpened] = useState(false);
+    const [category, setCategory] = useState('all');
+    const toggleContext = () => setContextOpened(!contextOpened);
+    const select = (e) => {
+        const category = e.currentTarget.dataset.category;
+        setCategory(category);
+        requestAnimationFrame(toggleContext);
+    };
+
+    const categories = [... new Set(menu.flatMap(x => x.categories))].sort();
+
     return (
         <Panel id={id}>
             {/*{fetchedUser &&*/}
@@ -90,9 +103,37 @@ const Home = ({id, go, fetchedUser}) => {
             {/*    </Cell>*/}
             {/*</Group>}*/}
 
+            <PanelHeader>
+                <PanelHeaderContent aside={<Icon16Dropdown />} onClick={toggleContext}>
+                    {category === 'all' ? 'Все темы' : category}
+                </PanelHeaderContent>
+            </PanelHeader>
+            <HeaderContext opened={contextOpened} onClose={toggleContext}>
+                <List>
+                    <Cell
+                        before={<Icon24Article />}
+                        // asideContent={category === 'all' ? <Icon24Done fill="var(--accent)" /> : null}
+                        onClick={select}
+                        data-category='all'
+                    > Все </Cell>
+                    {
+                        categories.map(category => (
+                            <Cell
+                                before={<Icon24Article />}
+                                // asideContent={category === 'all' ? <Icon24Done fill="var(--accent)" /> : null}
+                                onClick={select}
+                                data-category={category}
+                            >
+                                {category}
+                            </Cell>
+                        ))
+                    }
+                </List>
+            </HeaderContext>
+
             <Group title="Твоя подборка материалов">
                 <Div>
-                    {getMaterials()}
+                    <Materials mode={category} />
                 </Div>
             </Group>
         </Panel>
