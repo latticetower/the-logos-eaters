bd07d1a79b7a363b34afdacb9a7d36af6ff8cfbd
Michael Gamov <nofater@gmail.com>
2019-09-28 14:35:08 +0300

    Updates

---
+++ precache-manifest.c971962fdbeea53b16e48dca29762f28.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53b5e02575d3a5d90b0d4c5f6d6a9f60",
+    "url": "/the-logos-eaters/index.html"
+  },
+  {
+    "revision": "6d3fda342661411e0ea8",
+    "url": "/the-logos-eaters/static/css/2.4e89ce74.chunk.css"
+  },
+  {
+    "revision": "d213df5fb3ab362ec726",
+    "url": "/the-logos-eaters/static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "6d3fda342661411e0ea8",
+    "url": "/the-logos-eaters/static/js/2.6c5d12bd.chunk.js"
+  },
+  {
+    "revision": "d213df5fb3ab362ec726",
+    "url": "/the-logos-eaters/static/js/main.9dc86ea2.chunk.js"
+  },
+  {
+    "revision": "c6797e8962d39f754778",
+    "url": "/the-logos-eaters/static/js/runtime-main.7ef7bc6c.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/the-logos-eaters/static/media/persik.4e1ec840.png"
+  }
+]);
