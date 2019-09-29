0813c5f7d172df37dbf9e0d8fc08a93cbbc5b517
Tatiana Malygina <merlettaia@gmail.com>
2019-09-29 09:35:33 +0300

    Updates

--- precache-manifest.0b6aa54a935e451806b87fae31283bb5.js
+++ precache-manifest.0b6aa54a935e451806b87fae31283bb5.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "9615b8856c9b3fe45030c61f6992272d",
+    "revision": "97d7d08a7c6a0c26661d06019c7e8af0",
     "url": "/the-logos-eaters/index.html"
   },
   {
-    "revision": "e212af0ba328e9059b3b",
+    "revision": "c2fd5d5d60d5549514d4",
     "url": "/the-logos-eaters/static/css/2.4e89ce74.chunk.css"
   },
   {
-    "revision": "f0a104ffa820423acab0",
+    "revision": "080ad52e93d0dcdee313",
     "url": "/the-logos-eaters/static/css/main.f07e9a42.chunk.css"
   },
   {
-    "revision": "e212af0ba328e9059b3b",
-    "url": "/the-logos-eaters/static/js/2.60bae7fc.chunk.js"
+    "revision": "c2fd5d5d60d5549514d4",
+    "url": "/the-logos-eaters/static/js/2.32a90a1b.chunk.js"
   },
   {
-    "revision": "f0a104ffa820423acab0",
-    "url": "/the-logos-eaters/static/js/main.5cb14d11.chunk.js"
+    "revision": "080ad52e93d0dcdee313",
+    "url": "/the-logos-eaters/static/js/main.093b02b0.chunk.js"
   },
   {
     "revision": "135a96c122f52e4c2d89",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/the-logos-eaters/precache-manifest.2a851eeabfa7ac20ea56976fef413e61.js"
+  "/the-logos-eaters/precache-manifest.0b6aa54a935e451806b87fae31283bb5.js"
 );
 
 self.addEventListener('message', (event) => {
